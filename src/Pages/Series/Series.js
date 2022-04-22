bc7ad188192a02edb01e365b7d3e22b672ce820a
AhamedRasmi <ahamedrasmi025@gmail.com>
2022-04-23 03:20:43 +0530

    two

--- src/Pages/Series/Series.js
+++ src/Pages/Series/Series.js
@@ -31,7 +31,7 @@ const Series = () =>{
 
     return(
         <div>  
-           <span className ='pageTitle'>Series</span>
+           <span className ='pageTitle'> Tv Series</span>
              <Genres type = "tv"
              selectedGenres = {selectedGenres}
              setSelectedGenres = {setSelectedGenres}
